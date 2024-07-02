9530e026f419e9da197d5a674a3e58ec6d88ec70
mausv22h11 <mausv22h11@gmail.com>
2024-07-02 17:26:34 +0000

    second day of one hundred days of coding with python

---
+++ replit-challenges/day2.py
@@ -0,0 +1,43 @@
+# Day 2: Defining variables, saving their values, and displaying their contents.
+
+# Code of practice:
+print("Definitely not a Phishing Scam")
+print()
+myName = input("What is your name?")
+print(myName)
+cardNumber = input("Waht is your credit card number?")
+print("Thanks, I definitely won't put")
+print(cardNumber)
+print("into Amazon and order anything weird")
+print()
+print("Promise")
+maidenName = input("What is your Mother's maiden name? ")
+print()
+print("That's cool, I just wanted to know that")
+print(maidenName)
+print("was your Mum's maiden name. Not because the bank requested it or anything, honest.")
+
+# Challenge day 2:
+
+"""
+    Write code that does the following:
+
+    Asks for the user's name, favorite food, music and where they live
+    (or you can create other questions!)
+"""
+
+print("We should know you better:")
+print()
+name = input("What's your name? ")
+print("You are: ",name)
+food = input("What's your favorite food? ")
+print("You like to eat: ",food)
+music = input("What music do you like? ")
+print("You listen: ",music)
+pet = input("What's your furry friend's name? ")
+print("Your pet's name is: ",pet)
+habits = input("What are your habits? ")
+print("Your habits are: ",habits)
+print("Now we know you better!😊.")
+print("Uploading to database...")
+print("Our database has received your information. We appreciate your time.🤗")
